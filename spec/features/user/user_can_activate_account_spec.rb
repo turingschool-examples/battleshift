e0f1a5b4dab52e4ba48d428d579f153baa4c03a4
Anna Royer <anna.royer@colorado.edu>
2018-03-26 16:58:55 -0600

    add user visits dashboard and sees their account is active

--- spec/features/user/user_can_activate_account_spec.rb
+++ spec/features/user/user_can_activate_account_spec.rb
@@ -4,13 +4,16 @@ describe 'As a user' do
   context 'when I visit activate account link' do
     scenario 'I see a thank you page and my account is activated' do
       user = create(:user)
-      
-      expect(user.status).to eq('Inactive')
 
-      visit '/activate'
+      expect(user.status).to eq('inactive')
+
+      visit activate_path(user)
 
       expect(page).to have_content('Thank you! Your account is now activated.')
-      expect(user.status).to eq('Active')
+
+      visit dashboard_path
+
+      expect(page).to have_content('Status: Active')
     end
   end
 end
