0a54a372293bb4760a073b7d3f3aa926c04f0834
Andrew Dymond <andy.dymond@gmail.com>
2018-03-26 15:29:11 -0600

    remove url from activation_invite in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
 
   def activation_invite(user)
     @user = user
-    @url = activation_path(@user)
     mail(to: @user.email, subject: 'Activate your BattleShift account')
   end
 end
