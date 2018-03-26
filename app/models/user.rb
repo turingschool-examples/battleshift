fb4715f26d3a8728ed415115c8d6046fd57f9c40
Andrew Dymond <andy.dymond@gmail.com>
2018-03-26 14:45:25 -0600

    Add status & enum to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates_presence_of :password_confirmation, :password, :email, :name
   validates_confirmation_of :password, message: "Passwords must match like butter and astroturf"
   validates_uniqueness_of :email
+  enum status: [:inactive, :active]
 end
