098abdf324ea90b23d8c31503dd0a84d54e4e174
Andrew Dymond <andy.dymond@gmail.com>
2018-03-26 16:59:02 -0600

    Add user_can_activate_account_spec

---
+++ spec/features/user_can_activate_account_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "as unactivated user" do
+  it "displays inactive status" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    expect(page).to have_content("Status: Inactive")
+  end
+
+  xit "allows user to activate account" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit activation_path(user)
+    click_on "Activate my account"
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Thank you! Your account is now activated.")
+    expect(page).to have_content("Status: Active")
+  end
+end
