d91fe90f93dd2459f42bd99e944f8ece5863800b
Anna Royer <anna.royer@colorado.edu>
2018-03-26 15:44:15 -0600

    add mailing to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      RegistrationMailer.activate(@user).deliver_now
       redirect_to dashboard_path
     else
       render :new
