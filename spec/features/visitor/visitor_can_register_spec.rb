4a387fc97e9689034bf5c0aa3d7e8eb11966e32a
amj133 <amj@vt.edu>
2018-03-26 13:49:33 -0600

    remove user story comment from bottom of spec

--- spec/features/visitor/visitor_can_register_spec.rb
+++ spec/features/visitor/visitor_can_register_spec.rb
@@ -41,16 +41,3 @@ describe "As a visitor" do
     end
   end
 end
-
-
-# As a guest user
-# When I visit "/"
-# And I click "Register"
-# Then I should be on "/register"
-# And when I fill in an email address (required)
-# And I fill in name (required)
-# And I fill in password and password confirmation (required)
-# And I click submit
-# Then I should be redirected to "/dashboard"
-# And I should see a message that says "Logged in as <SOME_NAME>"
-# And I should see "This account has not yet been activated. Please check your email."
