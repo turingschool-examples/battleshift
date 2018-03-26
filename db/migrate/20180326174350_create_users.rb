6b855abb78dd772a762547a23a4e98048e598da5
Anna Royer <anna.royer@colorado.edu>
2018-03-26 17:44:03 -0600

    add api key to user table

--- db/migrate/20180326174350_create_users.rb
+++ db/migrate/20180326174350_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :email
       t.string :password_digest
       t.integer :status, default: 0
+      t.string :api_key
 
       t.timestamps
     end
