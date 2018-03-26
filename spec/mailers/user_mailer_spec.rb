a4c2e1a36a1f5815f2b3ef669600009224b8cefc
Andrew Dymond <andy.dymond@gmail.com>
2018-03-26 15:27:38 -0600

    Update user factory

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 describe UserMailer, type: :mailer do
   context "as a non-activated user" do
     it "invites new user to activate their account with registration email" do
-      email = UserMailer.activation_invite('player@example.com')
+      user = create(:user)
+      email = UserMailer.activation_invite(user)
 
       email.deliver_now
 
       expect(email.from).to eq('donotreply@battlshift.com')
-      expect(email.to).to eq('player@example.com')
+      expect(email.to).to eq(user.email)
       expect(email.subject).to eq('Activate your BattleShift account')
       expect(email.body).to eq(File.open('./spec/fixtures/user_mailer/activation.html'))
     end
