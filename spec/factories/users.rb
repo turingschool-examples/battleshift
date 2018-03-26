e725f24f247190aa6dea1f4d61efb38d1352fd54
Luke Chambers <lukenatchambers@gmail.com>
2018-03-26 13:24:23 -0600

    Build out migrations

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name "Opakawagalaga Eupanifahorious"
+    email "Opakawagalaga@Eupanifahorious.com"
+    password_digest "Opakawagalaga's Wife"
+  end
+end
