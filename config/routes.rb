4aa521584f6ffdaa5fbaa9fbca8ae1bb5014e9ee
katyjane8 <katy.jane8@gmail.com>
2018-03-26 17:55:11 -0600

    create activation route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   get "/register", to: "user#new"
   post "/register", to: "user#create"
-
   get "/dashboard", to: "user#show"
+  
+  get "/activate", as: "activate", to: "activation#new"
 
   namespace :api do
     namespace :v1 do
