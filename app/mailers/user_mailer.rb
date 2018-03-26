9258fcabd415d63e522144be3015eee78286e23e
Andrew Dymond <andy.dymond@gmail.com>
2018-03-26 16:22:45 -0600

    Add user mailer with activation_invite action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'donotreply@battlshift.com'
+  default from: 'donotreply@battleshift.com'
 
   def activation_invite(user)
     @user = user
+    @url = "#{ENV['root']}/activate/#{@user.id}"
     mail(to: @user.email, subject: 'Activate your BattleShift account')
   end
 end
