2cfd1cf5a961de9379f4842da3c5289aca3e50e8
amj133 <amj@vt.edu>
2018-03-27 17:25:06 -0600

    add before actions valid_api_key and require_api_key to api_controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,16 @@
 class ApiController < ActionController::API
+  before_action :require_api_key
+
   def require_game
     render status: 400 if Game.where(id: params[:id]).empty?
+  end
+
+  def require_api_key
+    render status: 401, json: {message: "Unauthorized"} unless valid_api_key?
+  end
+
+  def valid_api_key?
+    users = User.where(api_key: request.headers["X-API-KEY"])
+    users.first && users.first.active?
   end
 end
