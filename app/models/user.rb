3cb08a65072a2241e0e90b62395a12951983c571
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-27 00:31:18 -0600

    Add enum to user and status validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email, :password, :password_confirmation
+  validates_presence_of :name, :email, :password, :status
+  enum status: ["inactive", "active"]
 end
