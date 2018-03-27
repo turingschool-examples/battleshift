e8f7d4adcd8527f294cbd602efaf8fdc89eb1324
Anna Royer <anna.royer@colorado.edu>
2018-03-27 15:04:18 -0600

    add module for games

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :games, only: [:show, :create] do
+      resources :games, only: [:show, :create], module: :games do
         resources :shots, only: [:create]
+        resources :ships, only: [:create]
       end
     end
   end
