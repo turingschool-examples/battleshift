4e551c53d0fdb96fb3f535ed8a042d192162bb00
Andrew Dymond <andy.dymond@gmail.com>
2018-03-26 18:16:29 -0600

    Add user activation functionality

--- spec/features/user_can_activate_account_spec.rb
+++ spec/features/user_can_activate_account_spec.rb
@@ -15,7 +15,6 @@ describe "as unactivated user" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit activation_path(user)
-    click_on "Activate my account"
 
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Thank you! Your account is now activated.")
