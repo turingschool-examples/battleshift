9de902ef4e068212f42f4bb4d1b79fa2b55de7b1
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 15:12:19 -0600

    Add #game_invite test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 describe UserMailer, type: :mailer do
-  context "as a non-activated user" do
-    it "invites new user to activate their account with registration email" do
+  context "actions" do
+    it "#activation_invite" do
       user = create(:user)
       email = UserMailer.activation_invite(user)
 
@@ -14,6 +14,20 @@ describe UserMailer, type: :mailer do
       expect(email.body.encoded).to include("Thanks for signing up #{user.name}!")
       expect(email.body.encoded).to include("Click here to activate your account")
       expect(email.body.encoded).to include("Your api key is: #{user.id}, please keep it secret.")
+    end
+
+    it "#game_invite" do
+      user = create(:user, status: 1)
+      user.games.create()
+      email = UserMailer.game_invite("fake@fake.com", user)
+
+      email.deliver_now
+
+      expect(email.from).to eq(['donotreply@battleshift.com'])
+      expect(email.to).to eq(['fake@fake.com'])
+      expect(email.subject).to eq("You have been invited to a game of BattleShift!")
+      expect(email.body.encoded).to include("#{user.name} has invited you to a game of BattleShift!")
+      expect(email.body.encoded).to include("Join Game")
     end
   end
 end
