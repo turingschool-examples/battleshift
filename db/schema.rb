fd3a00bdef925f8d3d34be1d8f33962fc5aba488
katyjane8 <katy.jane8@gmail.com>
2018-03-27 14:14:14 -0600

    schema changed to add api key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327170021) do
+ActiveRecord::Schema.define(version: 20180327193938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20180327170021) do
     t.bigint "user_id"
     t.string "activation_key"
     t.index ["user_id"], name: "index_activation_keys_on_user_id"
+  end
+
+  create_table "api_keys", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_api_keys_on_user_id"
   end
 
   create_table "games", force: :cascade do |t|
