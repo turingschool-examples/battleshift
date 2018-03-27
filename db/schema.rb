4bded1438f977ce048a0dc900f3afcd449401ef6
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-27 00:30:34 -0600

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326210304) do
+ActiveRecord::Schema.define(version: 20180327043903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20180326210304) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_key"
+    t.integer "status", default: 0
   end
 
 end
