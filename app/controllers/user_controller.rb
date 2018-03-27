519ed844e34c9ce5779a7bde9572b24b3e097eff
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-26 21:11:29 -0600

    Add user new action and pass instance variable through show action.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,21 @@
 class UserController < ApplicationController
+
+  def new
+  end
+
   def create
     user = User.new(user_params)
     if user.save!
       ActivationMailer.activation_email(user).deliver_now
       session[:user_id] = user.id
-      redirect_to "/dashboard"
+      redirect_to dashboard_path
     else
       render :new
     end
   end
 
   def show
+    @user = current_user
   end
 
   private
