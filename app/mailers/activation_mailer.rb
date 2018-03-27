7f6650b1773b2cca773a8bfcce8cb111f047bff5
katyjane8 <katy.jane8@gmail.com>
2018-03-27 14:26:27 -0600

    activation url change

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -3,7 +3,7 @@ class ActivationMailer < ApplicationMailer
 
   def activation_email(user)
     @user = user
-    @url = "http://localhost:3000/activate?activation_key=#{@user.activation_key.activation_key}"
+    @url = activate_url(activation_key: @user.activation_key.activation_key)
     mail(to: @user.email, subject: 'Welcome! Please complete registration') do |format|
       format.text
       format.html
