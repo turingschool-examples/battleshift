d52cb79859c346b9d92ddc5196cbf5a2a787bd28
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 14:09:33 -0600

    Add games_controller with new, create & show actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,20 @@
 class GamesController < ApplicationController
 
+  def show
+    @game = Game.find(params[:id])
+  end
+
   def new
-    @game = Game.new
+  end
+
+  def create
+    new_game = current_user.games.new(player_1_board: Board.new(params[:difficulty].to_i), player_2_board: Board.new(params[:difficulty].to_i))
+    if new_game.save
+      UserMailer.game_invite(params[:email])
+      redirect_to game_path(new_game)
+    else
+      render :new
+    end
   end
 
 end
