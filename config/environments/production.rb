bc9f600a4362b760176c3086e9efc8575c25c6a4
amj133 <amj@vt.edu>
2018-03-26 19:32:36 -0600

    set default url for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "battleship_web_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.default_url_options = { host: ''}
+  config.action_mailer.default_url_options = { host: 'https://lit-hollows-27475.herokuapp.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
