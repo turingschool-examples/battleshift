8b413900d781eb04b3531f4fb3b42d16e1fe99a0
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 14:10:40 -0600

    Add game_invite action for user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,8 @@ class UserMailer < ApplicationMailer
     @url = "#{ENV['root']}/activate/#{@user.id}"
     mail(to: @user.email, subject: 'Activate your BattleShift account')
   end
+
+  def game_invite(email)
+    mail(to: email, subject: "You have been invited to a game of BattleShift!")
+  end
 end
