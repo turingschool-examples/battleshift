a7f00f609c813788edce4cb1aa06de3c22ddbcec
amj133 <amj@vt.edu>
2018-03-27 17:52:12 -0600

    add player_1 and player_2 as attrs to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326174350) do
+ActiveRecord::Schema.define(version: 20180327234718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180326174350) do
     t.integer "current_turn", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "player_1"
+    t.string "player_2"
   end
 
   create_table "users", force: :cascade do |t|
