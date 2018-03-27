4ffb1d174cd1e8717a2dddb2faf8503f2b73b0d8
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 14:08:20 -0600

    Add user_can_create_game_spec

--- spec/features/user_can_create_game_spec.rb
+++ spec/features/user_can_create_game_spec.rb
@@ -3,17 +3,17 @@ require "rails_helper"
 describe "user can create game" do
   context "registered user" do
     it "can create game against another player" do
-      user = create(:user)
+      user = create(:user, status: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit dashboard_path
       click_on "New Game"
 
-      expect(current_path).to be("/games/new")
+      expect(current_path).to eq("/games/new")
 
-      fill_in "Board size", with: 4
+      select "Easy", from: "Difficulty"
       fill_in "Opponent email", with: "test@email.com"
-      click_on "Create Game"
+      click_on "Invite your Opponent"
 
       expect(current_path).to eq("/games/1")
       expect(page).to have_content("Place your ships")
