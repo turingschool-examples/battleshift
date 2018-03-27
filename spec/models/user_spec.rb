e76b753fcea037979eac625a77db3ed6f595f31e
Luke Chambers <lukenatchambers@gmail.com>
2018-03-26 20:40:14 -0600

    Minor refactor

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_secure_password }
-  it { should validate_presence_of(:password_confirmation) }
   it { should validate_presence_of(:password) }
   it { should validate_confirmation_of(:password) }
   it { should validate_presence_of(:email) }
