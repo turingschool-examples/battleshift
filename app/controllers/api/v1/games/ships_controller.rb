8eecc6b10f710fae0f68ac76d7cff9f6e14ac040
amj133 <amj@vt.edu>
2018-03-27 15:43:41 -0600

    change initialize argument in ship_placer to hash

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::Games::ShipsController < ApiController
   def create
     game = Game.find(params[:game_id])
     ship = Ship.new(params[:ship_size].to_i)
-    ship_placer = ShipPlacer.new(game.player_1_board, ship, params[:start_space], params[:end_space])
+    ship_placer = ShipPlacer.new({
+                    board: game.player_1_board,
+                    ship: ship,
+                    start_space: params[:start_space],
+                    end_space: params[:end_space]
+                    })
     ship_placer.run
     game.save
     render json: game, message: ship_placer.message
