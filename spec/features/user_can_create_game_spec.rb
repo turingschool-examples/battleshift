f914e952da2d3d9da139bea4befbe3178e530e5d
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 14:50:32 -0600

    Add can't create game against unregistered user to user_can_create_game_spec

--- spec/features/user_can_create_game_spec.rb
+++ spec/features/user_can_create_game_spec.rb
@@ -21,6 +21,23 @@ describe "user can create game" do
       expect(page).to have_link("Finished turn")
     end
 
+    it "can't create game against unregistered user" do
+      user = create(:user, status: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+      click_on "New Game"
+
+      expect(current_path).to eq("/games/new")
+
+      select "Easy", from: "Difficulty"
+      fill_in "Opponent email", with: "random@person.com"
+      click_on "Invite your Opponent"
+
+      expect(current_path).to eq("/dashboard")
+      expect(page).to have_content("We invited your friend @ random@person.com to join Battleshift!")
+    end
+
 
   end
 end
