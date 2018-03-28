76609e3b3a41312104bd7f73d5071e169f732f3e
amj133 <amj@vt.edu>
2018-03-28 12:17:51 -0600

    initialize turn processor with a opponent board - as defined in shots controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::Games::ShotsController < ApiController
   def create
     game = Game.find(params[:game_id])
 
-    # opponent_board = case request.headers['X-API-KEY']
-    #                  when game.player_1 then game.player_2_board
-    #                  when game.player_2 then game.player_1_board
-    #                  end
+    opponent_board = case request.headers['X-API-KEY']
+                     when game.player_1 then game.player_2_board
+                     when game.player_2 then game.player_1_board
+                     end
 
-    turn_processor = TurnProcessor.new(game, params[:shot][:target])
+    turn_processor = TurnProcessor.new(game, params[:shot][:target], opponent_board)
 
     turn_processor.run!
     render json: game, message: turn_processor.message
--- app/services/turn_processor.rb
+++ app/services/turn_processor.rb
@@ -1,8 +1,9 @@
 class TurnProcessor
-  def initialize(game, target)
+  def initialize(game, target, opponent_board)
     @game   = game
     @target = target
     @messages = []
+    @opponent_board = opponent_board
   end
 
   def run!
@@ -21,10 +22,10 @@ class TurnProcessor
 
   private
 
-  attr_reader :game, :target
+  attr_reader :game, :target, :opponent_board
 
   def attack_opponent
-    result = Shooter.fire!(board: opponent.board, target: target)
+    result = Shooter.fire!(board: opponent_board, target: target)
     @messages << "Your shot resulted in a #{result}."
     game.player_1_turns += 1
   end
