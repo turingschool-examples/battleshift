5d9c0890e2f215582041b6d425d27241f4ecfa39
amj133 <amj@vt.edu>
2018-03-28 12:18:01 -0600

    shots spec passing

--- spec/requests/api/v1/games/shots_spec.rb
+++ spec/requests/api/v1/games/shots_spec.rb
@@ -73,12 +73,12 @@ describe "Api::V1::Shots" do
     end
 
     it "updates the message but not the board with invalid coordinates" do
-      player_1_board = Board.new(1)
-      player_2_board = Board.new(1)
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      # player_1_board = Board.new(1)
+      # player_2_board = Board.new(1)
+      # game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
 
-      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => user.api_key}
-      json_payload = {target: "B1"}.to_json
+      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => player_1.api_key}
+      json_payload = {target: "B5"}.to_json
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: headers
 
       game = JSON.parse(response.body, symbolize_names: true)
