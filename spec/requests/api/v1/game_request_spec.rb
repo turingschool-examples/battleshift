2610fcbec0a17c1a2fa70f7b81b4a161a5aee310
Anna Royer <anna.royer@colorado.edu>
2018-03-28 08:23:45 -0600

    add specs for locking down with api keys

--- spec/requests/api/v1/game_request_spec.rb
+++ spec/requests/api/v1/game_request_spec.rb
@@ -2,13 +2,19 @@ require 'rails_helper'
 
 describe "post /api/v1/game" do
   it "creates a new game" do
-    user = create(:user)
+    player = create(:player)
+    opponent = create(:opponent)
 
-    post "/api/v1/games", :headers => {"X-API-KEY" => user.api_key}
+    payload = {opponent_email: opponent.email}
+    headers = {"X-API-KEY" => player.api_key}
+
+    post "/api/v1/games", params: payload, :headers => headers
 
     actual_game = JSON.parse(response.body, symbolize_names: true)
     expected_game = Game.last
 
     expect(expected_game.id).to eq(actual_game[:id])
+    expect(expected_game.player_1).to eq(player.api_key)
+    expect(expected_game.player_2).to eq(opponent.api_key)
   end
 end
