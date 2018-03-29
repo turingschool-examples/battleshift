343b2f7211b94182650677d3c24085bf8c4ebe38
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-28 20:35:46 -0600

    Add user association to game to games controller create

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -16,9 +16,12 @@ module Api
       def create
         game = Game.create(player_1_board: Board.new(4),
                            player_2_board: Board.new(4))
+        user_1_api_key = response.request.env["HTTP_X_API_KEY"]
+        player_1_lookup = ApiKey.find_by(api_key: user_1_api_key).user
+        player_2_lookup = User.find_by(email: params[:opponent_email])
+        GamePlayer.create(player_1_id: player_1_lookup.id, player_2_id: player_2_lookup.id, game_id: game.id)
         render json: game
       end
     end
   end
 end
-# response.request.env["HTTP_X_API_KEY"]
