89adb1575d94196fafcbccaea8e7c260e5617876
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-30 01:02:32 -0600

    Add determine board method to ships controller to check for user's api key and specify player board.

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -3,23 +3,36 @@ module Api
     module Games
       class ShipsController < ApiController
         def create
-          game = current_game
+          @game = current_game
+          @player_board = determine_board
           ship_size = params[:ship]["ship_size"]
 
           shipplacer = ShipPlacer.new(
-                board: game.player_1_board,
+                board: @player_board,
                 ship: Ship.new(ship_size),
                 start_space: params[:ship]["start_space"],
                 end_space: params[:ship]["end_space"])
           shipplacer.run
 
-          game.save
+          @game.save
 
-          game.update(current_turn: 0)
+          @game.update(current_turn: 0)
 
-          game.update(messages: Message.new(current_game).placing_ships_messages(ship_size.to_i))
+          @game.update(messages: Message.new(current_game, @player_board).placing_ships_messages(ship_size.to_i))
 
-          render json: game
+          render json: @game
+        end
+
+        private
+
+        def determine_board
+          if @game.player_1.api_key.api_key == response.request.env["HTTP_X_API_KEY"]
+            @game.player_1_board
+          elsif @game.player_2.api_key.api_key == response.request.env["HTTP_X_API_KEY"]
+            @game.player_2_board
+          else
+            render status: 400
+          end
         end
       end
     end
