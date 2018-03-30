550aefcda817e4a94337065ee5fbf0b183038b4e
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-29 18:56:06 -0600

    Add message to json render, and pull out shipplacer instance in order to call its messages.

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -5,14 +5,14 @@ module Api
         def create
           game = Game.find(params[:id])
 
-          # game.player_1_board.set_space_occupied()
-          shipplacer = ShipPlacer.new(board: game.player_1_board,
-                          ship: Ship.new(params[:ship]["ship_size"]),
-                          start_space: params[:ship]["start_space"],
-                          end_space: params[:ship]["end_space"]).run
+          shipplacer = ShipPlacer.new(
+                board: game.player_1_board,
+                ship: Ship.new(params[:ship]["ship_size"]),
+                start_space: params[:ship]["start_space"],
+                end_space: params[:ship]["end_space"])
+          shipplacer.run
 
-
-          render json: game
+          render json: game, message: shipplacer.messages
         end
       end
     end
