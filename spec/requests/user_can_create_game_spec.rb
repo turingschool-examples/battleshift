53dda1c3b0a8a6b19435f5536c682c3340006a1b
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-29 22:45:53 -0600

    Remove lazy evaluation.

--- spec/requests/user_can_create_game_spec.rb
+++ spec/requests/user_can_create_game_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Api::V1::Games" do
   context "POST /api/v1/games" do
-  let(:user_1) {create(:user)}
-  let(:user_2) {create(:user, email: "ellen@ellen.com")}
     it "starts the game" do
+      user_1 = create(:user)
+      user_2 = create(:user, email: "ellen@ellen.com")
       user_1.api_key = create(:api_key, user: user_1)
       user_2.api_key = create(:api_key, api_key: "542345243642", user: user_2)
 
