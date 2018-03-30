a27c992b2b2450f49872b536315f6d9062225ec6
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-29 18:55:15 -0600

    Move setup to before block so response returns correct api endpoint. Fix expectations.

--- spec/requests/user_can_place_ships_spec.rb
+++ spec/requests/user_can_place_ships_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 describe "Api::V1::Games" do
   context "POST /api/v1/games/:id/ships" do
-    let(:user_1) {create(:user)}
-    let(:user_2) {create(:user, email: "ellen@ellen.com")}
 
-    it "places ships" do
+    before :all do
+      user_1 = create(:user)
+      user_2 = create(:user, email: "ellen@ellen.com")
       user_1.api_key = create(:api_key, user: user_1)
       user_2.api_key = create(:api_key, api_key: "542345243642", user: user_2)
       headers = { "X-API-KEY" => user_1.api_key.api_key,
-                  "CONTENT-TYPE" => "application/json" }
-      params = { :opponent_email => "#{user_2.email}" }.to_json
-      post "/api/v1/games", :params => params, :headers => headers
+        "CONTENT-TYPE" => "application/json" }
+        params = { :opponent_email => "#{user_2.email}" }.to_json
+        post "/api/v1/games", :params => params, :headers => headers
+    end
 
+    it "places ships" do
       ship_payload = {ship_size: 3, start_space: "A1", end_space: "A3"}
 
       post "/api/v1/games/#{Game.last.id}/ships", :params => { ship: ship_payload }
@@ -21,8 +23,10 @@ describe "Api::V1::Games" do
 
       expect(response).to be_success
 
-      # expect(game_data[:player_1_board][:rows].count).to eq(board_size)
-      expect(result[:message]).to include("Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2.")
+      result["player_1_board"]["rows"].first["data"][0..2].each do |a_row|
+        expect(a_row["contents"]).to_not be_nil
+      end
+      expect(result["message"]).to eq("Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2.")
     end
   end
 end
