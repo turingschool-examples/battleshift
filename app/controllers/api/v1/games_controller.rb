4ef659710c3142d589d9c22eabce5246fb305e14
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-30 01:00:31 -0600

    Update games controller to reflect new relationships.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -17,9 +17,9 @@ module Api
         game = Game.create(player_1_board: Board.new(4),
                            player_2_board: Board.new(4))
         user_1_api_key = response.request.env["HTTP_X_API_KEY"]
-        player_1_lookup = ApiKey.find_by(api_key: user_1_api_key).user
-        player_2_lookup = User.find_by(email: params[:opponent_email])
-        GamePlayer.create(player_1_id: player_1_lookup.id, player_2_id: player_2_lookup.id, game_id: game.id)
+        player_1 = ApiKey.find_by(api_key: user_1_api_key).user
+        player_2 = User.find_by(email: params[:opponent_email])
+        game.update(player_1_id: player_1.id, player_2_id: player_2.id)
         render json: game
       end
     end
