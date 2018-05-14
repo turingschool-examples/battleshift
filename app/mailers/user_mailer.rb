eaa0ce21ad4398965a600c8a8936a362df24b64c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-14 13:21:15 -0600

    Start of user activation email spec

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def activation_email
     @user = params[:user]
-    @url = "#{ENV['base_url']}/activate/#{@user.activation_code}"
+    @url = "#{ENV['base_url']}/activate/#{@user.activation_key}"
 
     mail(to: @user.email, subject: 'Activate Your Battleshift Account')
   end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  it 'should send an activation email' do
+  describe 'Activation Email' do
+    let(:user) { create(:user) }
+    let(:email) { UserMailer.with(user: user).activation_email }
 
+    it 'should send an activation email' do
+      expect(email.body.encoded).to have_link('Visit here to activate your account.')
+
+      link = find('a')
+      binding.pry
+    end
   end
 end
