99c25fece7e08267b6d9c3e87060a1493f2808a5
pollockcl <pollock.clee@gmail.com>
2018-05-14 16:44:52 -0600

    added model tests and methods for activation and base new method for activation controller

--- app/controllers/users/activation_controller.rb
+++ app/controllers/users/activation_controller.rb
@@ -1,7 +1,13 @@
 module Users
   class ActivationController < ApplicationController
-    def new
-      
+    def edit
+      user = User.find_by_id(params[:id])
+    end
+
+    def update
+      user = User.find_by_id(params[:id])
+      user.activate
+      redirect_to user
     end
   end
 end
