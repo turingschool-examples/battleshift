92eda341ab6dad8cd2f70588708c39049c724213
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-14 11:46:46 -0600

    initial setup with bcrypt, add users table

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Logged in as #{user.name}"
+      redirect_to '/dashboard'
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
