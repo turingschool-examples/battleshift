cbab0055a0cb68fb24014fedca3e72518c0b88d4
pollockcl <pollock.clee@gmail.com>
2018-05-14 13:50:27 -0600

    added validation tests for game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:player_1_board) }
+    it { is_expected.to validate_presence_of(:player_2_board) }
+  end
 end
