10b66652f9306de834363bc6dbdd61f0c63ac352
pollockcl <pollock.clee@gmail.com>
2018-05-14 13:41:10 -0600

    fixed and appended failing test

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,9 +2,21 @@ module Api
   module V1
     class GamesController < ActionController::API
       def show
-        game = Game.find(params[:id])
-        render json: game
+        game = Game.find_by_id(params[:id])
+        if game
+          render json: game
+        else
+          render json: not_found, status: 400  
+        end
       end
+
+
+      private
+
+        def not_found
+          { status: 400,
+            message: 'Game does not exist' }.to_json
+        end
     end
   end
 end
