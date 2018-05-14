f6c3c98c77e99119035ed112b6081897a14af010
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-14 13:06:42 -0600

    User has activation keys generated upon creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ describe User, type: :model do
   it { is_expected.to validate_confirmation_of(:password) }
 
   it 'should not be active by default and have an activation key' do
-    user = User.new(name: 'a', email: 'b', password: '1', password_confirmation: '1')
+    user = User.create(name: 'a', email: 'b', password: '1', password_confirmation: '1')
 
     expect(user.activated).to be_falsey
     expect(user.activation_key).to_not be_nil
