3d4787d71fe485ba1f4c3bacede5c3ce86130e2e
JNelson <nelson.jimmy@gmail.com>
2018-05-14 13:42:03 -0600

    Action mailer shows body

--- spec/features/user_can_activate_their_account_spec.rb
+++ spec/features/user_can_activate_their_account_spec.rb
@@ -5,12 +5,15 @@ describe 'User' do
     scenario 'and can click the link' do
       user = User.create(name: 'Jimmy', email: 'nelson.jimmy@gmail.com', password: 'password', password_confirmation: 'password')
 
-      email = UserMailer.with(user: user).registration_email.deliver_now
+      UserMailer.with(user: user).registration_email.deliver_now
+
+      email = ActionMailer::Base.deliveries.last
 
       expect(email.from).to eq(['no-reply@battleshift.com'])
       expect(email.to).to eq([user.email])
       expect(email.subject).to eq('Welcome to Battleshift!')
-      expect(email.body).to have_content('Visit here to activate your account.')
+      expect(email.html_part.body.to_s).to have_content('Visit here to activate your account.')
+      expect(email.text_part.body.to_s).to have_content('Visit here to activate your account.')
     end
   end
 end
