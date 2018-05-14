88e8c3b2bb953ddefcea49909922446b47fa8c58
alexcutschall <alexander.cutschall@gmail.com>
2018-05-14 14:30:29 -0600

    adding in api key

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
   def activation_email
     @user = params[:user]
     @url = "#{ENV['base_url']}/activate/#{@user.activation_key}"
-
+    @api_key = @user.api_key
+    
     mail(to: @user.email, subject: 'Activate Your Battleshift Account')
   end
 end
---
+++ db/migrate/20180514201851_add_api_keyto_user.rb
@@ -0,0 +1,5 @@
+class AddApiKeytoUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :api_key, :string
+  end
+end
