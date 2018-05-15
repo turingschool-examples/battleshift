1a9292174108377b19496a64279abad18cee3a7f
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-15 15:03:02 -0600

    Pushing info across.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class GamesController < ActionController::API
+      def index
+        render json: Game.all
+      end
+
       def show
         begin
           game = Game.find(params[:id])
@@ -8,6 +12,11 @@ module Api
         rescue ActiveRecord::RecordNotFound
           render nothing: true, status: 400
         end
+      end
+
+      def create
+        require 'pry'; binding.pry
+        render json: Game.new()
       end
     end
   end
