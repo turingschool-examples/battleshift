9144e7a61a1c829f5ee3d95c110f89201173b584
alexcutschall <alexander.cutschall@gmail.com>
2018-05-15 10:11:14 -0600

    passing tests

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,15 +2,25 @@ module Api
   module V1
     class GamesController < ActionController::API
       def show
-        game = Game.find(params[:id])
-        render json: game
+        begin
+          game = Game.find(params[:id])
+          render json: game
+        rescue ActiveRecord::RecordNotFound
+          render nothing: true, status: 400
+        end
       end
 
       def create
-        # TODO: create first player
-        game = Game.new
-        render json: game
+        token = request.env["HTTP_X_API_KEY"]
+        if User.find_by(api_key: token) != nil
+          player_1_board = Board.new(4)
+          player_2_board = Board.new(4)
+          game = Game.create(player_1_board: player_1_board, player_2_board: player_2_board)
+
+          render json: game
+        end
       end
+
     end
   end
 end
--- spec/features/user/user_can_start_a_new_game_spec.rb
+++ spec/features/user/user_can_start_a_new_game_spec.rb
@@ -6,18 +6,16 @@ describe 'as player 1' do
       user = create(:activated_user)
       user_2 = create(:activated_user_2)
 
-      conn = Faraday.new(:url => 'localhost:3000')
+      conn = Faraday.new(:url => 'http://localhost:3000')
 
       response = conn.post do |req|
         req.url '/api/v1/games'
         req.headers['Content-Type'] = 'application/json'
-        req.headers['Authorization'] = "#{user.api_key}"
+        req.headers['X-API-Key'] = ENV["BATTLESHIFT_API_KEY"]
+        req.body = '{ "player_2_email": "foo21@foo.com" }'
       end
 
-      binding.pry
-
       expect(response).to be_success
-      #test that both are in the system
     end
   end
 end
