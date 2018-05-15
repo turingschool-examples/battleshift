4326b1835883e5ec330626ab8005a4c9392f14af
Nikhil Shahi <shniks@gmail.com>
2018-05-15 10:34:03 -0600

    Added views to login and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get '/register',  to: 'users#new'
   post '/register', to: 'users#create'
   get '/dashboard', to: 'users#show'
