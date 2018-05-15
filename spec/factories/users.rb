396218bd1f48fca369555804ba2b00f51bb3b625
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-15 10:26:17 -0600

    Refactors to clean up code

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     sequence(:email) { |n| "foo#{n}@foo.com" }
     password "password"
     password_confirmation "password"
-    api_key "VN81BL5DRAK704A5O41HG0Q8RGA1VSH5O"
+    sequence(:api_key) { |n| "API_KEY_#{n}" }
   end
 
   factory :activated_user, class: User do
@@ -13,14 +13,6 @@ FactoryBot.define do
     password "password"
     password_confirmation "password"
     activated true
-    api_key "VN81BL5DRAK704A5O41HG0Q8RGA1VSH5O"
-  end
-  factory :activated_user_2, class: User do
-    sequence(:name) { |n| "User #{n}" }
-    sequence(:email) { |n| "foo2#{n}@foo.com" }
-    password "password"
-    password_confirmation "password"
-    activated true
-    api_key "VN81BL5DRAK704A5O41HG0Q8RGA1VSH5O"
+    sequence(:api_key) { |n| "API_KEY_#{n}" }
   end
 end
--- spec/features/user/user_can_start_a_new_game_spec.rb
+++ spec/features/user/user_can_start_a_new_game_spec.rb
@@ -3,16 +3,15 @@ require 'rails_helper'
 describe 'as player 1' do
   context 'send a request to player 2' do
     it 'creates a game' do
-      user = create(:activated_user)
-      user_2 = create(:activated_user_2)
+      user, user2 = create_list(:activated_user, 2)
 
       conn = Faraday.new(:url => 'http://localhost:3000')
 
       response = conn.post do |req|
         req.url '/api/v1/games'
         req.headers['Content-Type'] = 'application/json'
-        req.headers['X-API-Key'] = ENV["BATTLESHIFT_API_KEY"]
-        req.body = '{ "player_2_email": "foo21@foo.com" }'
+        req.headers['X-API-Key'] = user.api_key
+        req.body = { 'player_2_email': user2.email }.to_json
       end
 
       expect(response).to be_success
