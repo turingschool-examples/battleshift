7c09b9699db16b3bc3580c67b4de2143f9aa13ca
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-15 08:35:46 -0600

    Adding tests.

--- spec/features/user/user_receives_registration_email_spec.rb
+++ spec/features/user/user_receives_registration_email_spec.rb
@@ -4,14 +4,18 @@ feature 'Registration' do
   scenario 'user receives confirmation email post registery' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    # Background: The registration process will trigger this story
 
-    # visit dashboard_path
-    #
-    # expect(page).to have_content('This account has not yet been activated')
+    visit dashboard_path
+
+    click_on 'Login'
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+
+    click_on 'Submit'
+    expect(current_path).to eq(dashboard_path)
 
     visit "/activate?id=#{user.id}"
-    require 'pry'; binding.pry
 
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content('Status: Active')
