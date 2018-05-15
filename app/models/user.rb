ee2b6cf619667037bc1d3fab29e73d96e99de4e3
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-14 19:21:59 -0600

    Adds api_key production to user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   has_secure_password
   enum status: ['inactive', 'active']
+  before_save :key_gen
+
+  def key_gen
+    self.api_key = SecureRandom.urlsafe_base64
+  end
 end
