d8769bd320f2832c050d8c9cdb7a6d8703580877
pollockcl <pollock.clee@gmail.com>
2018-05-15 08:38:49 -0600

    added callback method to generate a random api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :name, :password_digest
   validates :email, presence: true, uniqueness: true
+  before_create :set_api_token
 
   def activate!
     update(activated: true)
@@ -11,13 +12,20 @@ class User < ApplicationRecord
     activated
   end
 
-  class << self
 
-    def find_and_activate(id)
-      user = find(id)
-      user.activate!
-      user
+  def self.find_and_activate(id)
+    user = find(id)
+    user.activate!
+    user
+  end
+  
+
+  private
+
+  def set_api_token
+    self.api_key = loop do
+      token = SecureRandom.hex(20)
+      break token unless User.exists?(api_key: token)
     end
-
   end
 end
