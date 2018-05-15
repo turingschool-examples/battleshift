17b76c93ba271ec9a2e2ecfd43681bf3ca73ecb2
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-14 19:37:08 -0600

    Adding user model methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   has_secure_password
   enum status: ['inactive', 'active']
-  before_save :key_gen
+  before_create :key_gen
 
   def key_gen
     self.api_key = SecureRandom.urlsafe_base64
