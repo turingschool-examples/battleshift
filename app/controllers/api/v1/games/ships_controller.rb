7f08d2e61c60588eb27dcc1656815e6419eaa995
pollockcl <pollock.clee@gmail.com>
2018-05-15 16:25:09 -0600

    got a bit farther in the spec harness

---
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    module Games
+      class ShipsController < ActionController::API
+        def index 
+          # code here
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
     namespace :v1 do
         post '/games', to: 'games#create'
       resources :games, only: [:show] do
+        post '/ships', to: 'games/ships#index'
         post '/shots', to: 'games/shots#create'
       end
     end
