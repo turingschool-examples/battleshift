4829a53925ece2f40fa08f3761090fd92e437870
pollockcl <pollock.clee@gmail.com>
2018-05-15 09:56:37 -0600

    removed obsolete methods

--- app/controllers/users/activation_controller.rb
+++ app/controllers/users/activation_controller.rb
@@ -1,16 +1,8 @@
 module Users
   class ActivationController < ApplicationController
-    def edit
-      @user = User.find(params[:id])
-    end
-
     def update
       user = User.find_and_activate(params[:key])
       login(user)
-    end
-
-    def show
-      @user = User.find(params[:id])
     end
   end
 end
