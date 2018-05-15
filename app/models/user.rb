d05aac4a58a7ad25f01e3a4e43b2760a8d503ea8
agpiermarini <piermarini.andrew@gmail.com>
2018-05-14 18:04:49 -0600

    Add api_key migration and User#set_api_key and User#generate_api_key methods. Adds before_save action for set_api_key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, :username, presence: true, uniqueness: true
   validates :password, presence: true
+  before_save :set_api_key
+
+  private
+    def set_api_key
+      self.api_key = generate_api_key
+    end
+
+    def generate_api_key
+      SecureRandom.urlsafe_base64
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :username }
-  it { should validate_presence_of :password }
-  it { should validate_uniqueness_of :username }
-  it { should validate_uniqueness_of :email }
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :username }
+    it { should validate_uniqueness_of :email }
+  end
+
+  describe 'class methods' do
+    describe '#set_api_key and #generate_api_key' do
+      it 'generate an api key (in combination) before save' do
+        user = User.new(username: 'username', email: 'email@email.com', password: 'password')
+
+        expect(user.api_key).to be_nil
+
+        user.save!
+
+        expect(user.api_key).to_not be_nil
+      end
+    end
+  end
 end
