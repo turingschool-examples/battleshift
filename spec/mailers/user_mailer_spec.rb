7884518636b670d15b572c22b9581932c5b3bc12
Nikhil Shahi <shniks@gmail.com>
2018-05-15 05:37:24 -0600

    Updated the mailer views with the api_key. Added the api_key variable to user mailer.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe UserMailer, type: :mailer do
 
       expect(email.subject).to eq('Activate your Battleshift account')
       expect(email.to).to eq(["#{user.email}"])
-      expect(email.from).to eq(['from@example.com'])
+      expect(email.from).to eq(['welcome@battleshift.com'])
       expect(email.body).to include("http://localhost:3000/users/#{user.id}/activate")
       expect(email.body).to include(user.api_key)
     end
