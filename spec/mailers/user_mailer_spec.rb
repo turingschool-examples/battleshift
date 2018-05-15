0fcf4c427a5fa895b5f5cf6c89f99b9eef7cf607
Nikhil Shahi <shniks@gmail.com>
2018-05-15 02:39:19 -0600

    Added conditionals to users show view.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Registration email" do
+    it "includes activation link" do
+      user = create(:user)
+      email = UserMailer.registration_email(user)
+
+      binding.pry
+      expect(email.subject).to eq('Activate your Battleshift account')
+      expect(email.to).to eq(["#{user.email}"])
+      expect(email.from).to eq(['from@example.com'])
+      expect(email.body.encoded).to include("http://localhost:3000/activate/#{user.activation_key}")
+      expect(email.body.encoded).to include(user.api_key.id)
+    end
+  end
 end
