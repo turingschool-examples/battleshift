755cf202df6af5f036365586055b7ace2915e62f
Nikhil Shahi <shniks@gmail.com>
2018-05-17 14:37:13 -0600

    Added params for the ship class.

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -6,6 +6,7 @@ module Api
           game = Game.find(params[:game_id])
           turn_processor = TurnProcessor.new(game, params[:shot][:target])
           turn_processor.run!
+          game.save!
           render json: game, message: turn_processor.message
         end
       end
