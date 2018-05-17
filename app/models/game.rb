9351b8a8ba10ba0d2560db42ce3ea5ddd8baa5b0
pollockcl <pollock.clee@gmail.com>
2018-05-17 17:09:15 -0600

    added method to cycle a game turn

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,10 @@ class Game < ApplicationRecord
 
   belongs_to :player_1, class_name: "User", foreign_key: "player_1"
   belongs_to :player_2, class_name: "User", foreign_key: "player_2"
+  alias_attribute :p1, :player_1
+  alias_attribute :p2, :player_2
+  alias_attribute :p1_board, :player_1_board
+  alias_attribute :p2_board, :player_2_board
 
   enum current_turn: ["challenger", "computer"]
   serialize :player_1_board
@@ -16,12 +20,19 @@ class Game < ApplicationRecord
            player_2: opponent(params),
            player_1_board: Board.new(4),
            player_2_board: Board.new(4),
-           current_turn: [0, 1].sample)
+           current_turn: 1)
 
   end
 
   def turn_sum
     player_1_turns + player_2_turns
+  end
+
+  def cycle_turn
+    case self.current_turn
+    when 'challenger' then self.current_turn = 'computer'; save
+    when 'computer'   then self.current_turn = 'challenger'; save
+    end
   end
 
   private
