ed651d8382563555d4bdd0506844bbc82c31b0e9
Nikhil Shahi <shniks@gmail.com>
2018-05-17 12:56:24 -0600

    Created conditional for the board in the ships controller.

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -4,18 +4,11 @@ module Api
       class ShipsController < ActionController::API
         def index
           game = Game.find_by_id(params[:game_id])
-          if game.turn_sum.even?
-            if game.player_2_board.player.nil?
-              game.player_2_board.player(User.find_by_api_key(request.headers['X-API-key']))
-              #board = game.player_2_board
-            end
+          if request.headers['X-API-key'] == game.player_1.api_key
+            board = game.player_1_board
           else
-            if game.player_1_board.player.nil?
-              game.player_1_board.player(User.find_by_api_key(request.headers['X-API-key']))
-              #board = game.player_1_board
-            end
-          end
-
+            board = game.player_2_board
+          end 
           ship_placer = ShipPlacer.new(board: board, ship: Ship.new(params['ship_size']), start_space: params['start_space'], end_space: params['end_space'])
           message = ship_placer.message #call the message from the ShipPlacer
           render json: game, message: message
--- app/services/ship_placer.rb
+++ app/services/ship_placer.rb
@@ -16,17 +16,18 @@ class ShipPlacer
     end
   end
 
+  def message
+    if ship.length == 3
+      "Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2."
+    else
+      "Successfully placed ship with a size of 2. You have 0 ship(s) to place."
+    end
+  end
+  
   private
   attr_reader :board, :ship,
     :start_space, :end_space
 
-  def message
-   if ship.length == 3
-     "Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2."
-   else
-     "Successfully placed ship with a size of 2. You have 0 ship(s) to place."
-   end
-  end
 
   def same_row?
     start_space[0] == end_space[0]
