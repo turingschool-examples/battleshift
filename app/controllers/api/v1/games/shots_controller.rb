a7258e8c4ee2946e9959d29083668b7c01a16aff
pollockcl <pollock.clee@gmail.com>
2018-05-17 17:44:37 -0600

    players cant take two turns in a row

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -4,10 +4,25 @@ module Api
       class ShotsController < ApiController
         def create
           game = Game.find(params[:game_id])
-          turn_processor = TurnProcessor.new(game, params[:shot][:target])
-          turn_processor.run!
-          game.save!
-          render json: game, message: turn_processor.message
+          if bad_user(game)
+            render json: game, message: "Invalid move. It's your opponent's turn", status: 400 
+          else
+            turn_processor = TurnProcessor.new(game, params[:shot][:target])
+            turn_processor.run!
+            game.save!
+            render json: game, message: turn_processor.message
+          end
+        end
+
+        private
+
+        def cheater
+          { status: 400,
+            message: 'It is not your turn' }
+        end
+
+        def bad_user(game)
+          game.player_1_turns - game.player_2_turns == (2 || -2)
         end
       end
     end
