dbd27bea687fb873ca4bce29c0997490874518e5
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-18 10:56:14 -0600

    Out of turn test

--- spec/features/user/user_can_not_make_shot_out_of_turn_spec.rb
+++ spec/features/user/user_can_not_make_shot_out_of_turn_spec.rb
@@ -16,9 +16,8 @@ context 'A user posting to /api/v1/games/:id/shots out of turn' do
     @conn.post do |req|
       req.url "/api/v1/games/#{@game_json[:id]}/ships"
       req.headers['Content-Type'] = 'application/json'
-      req.headers['X-API-Key'] = ENV['BATTLESHIFT_API_KEY']
+      req.headers['X-API-Key'] = ENV['BATTLESHIFT_OPPONENT_API_KEY']
       req.body = {
-        'email': ENV['BATTLESHIFT_EMAIL'],
         'ship_size': 2,
         'start_space': "A1",
         'end_space': "A2"
@@ -36,6 +35,8 @@ context 'A user posting to /api/v1/games/:id/shots out of turn' do
 
     expect(response).to be_success
     data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:message]).to include('Invalid move. It\'s your opponent\'s turn')
 
   end
 end
