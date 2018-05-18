b61b2308f363715f0f6b7ae22a78a48ae7621889
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-18 10:13:19 -0600

    Shots can miss

--- spec/features/user/user_can_make_a_shot_spec.rb
+++ spec/features/user/user_can_make_a_shot_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 context 'A user posting to /api/v1/games/:id/shots' do
   describe 'can place a shot' do
-    scenario 'that hits' do
+    before(:all) do
+      @conn = Faraday.new(url: 'http://localhost:3000')
 
-      conn = Faraday.new(:url => 'http://localhost:3000')
-
-      response = conn.post do |req|
+      response = @conn.post do |req|
         req.url "/api/v1/games"
         req.headers['Content-Type'] = 'application/json'
         req.headers['X-API-Key'] = ENV['BATTLESHIFT_API_KEY']
         req.body = { 'opponent_email': ENV['BATTLESHIFT_OPPONENT_EMAIL'] }.to_json 
       end
 
-      game_json = JSON.parse(response.body, symbolize_names: true)
+      @game_json = JSON.parse(response.body, symbolize_names: true)
 
-      conn.post do |req|
-        req.url "/api/v1/games/#{game_json[:id]}/ships"
+      @conn.post do |req|
+        req.url "/api/v1/games/#{@game_json[:id]}/ships"
         req.headers['Content-Type'] = 'application/json'
         req.headers['X-API-Key'] = ENV['BATTLESHIFT_API_KEY']
         req.body = {
@@ -26,9 +25,11 @@ context 'A user posting to /api/v1/games/:id/shots' do
           'end_space': "A2"
         }.to_json
       end
+    end
 
-      response = conn.post do |req|
-        req.url "/api/v1/games/#{game_json[:id]}/shots"
+    scenario 'that hits' do
+      response = @conn.post do |req|
+        req.url "/api/v1/games/#{@game_json[:id]}/shots"
         req.headers['Content-Type'] = 'application/json'
         req.headers['X-API-Key'] = ENV['BATTLESHIFT_OPPONENT_API_KEY']
         req.body = { target: "A1" }.to_json
@@ -41,8 +42,18 @@ context 'A user posting to /api/v1/games/:id/shots' do
 
     end
 
-    xscenario 'that misses' do
+    scenario 'that misses' do
+      response = @conn.post do |req|
+        req.url "/api/v1/games/#{@game_json[:id]}/shots"
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['X-API-Key'] = ENV['BATTLESHIFT_OPPONENT_API_KEY']
+        req.body = { target: "D1" }.to_json
+      end
 
+      expect(response).to be_success
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:message]).to include("Your shot resulted in a Miss")
     end
   end
 end
