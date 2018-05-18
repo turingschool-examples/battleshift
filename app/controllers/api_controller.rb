f9abd12b6959d027bef15b1f425d7133395cf7b2
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-18 11:10:18 -0600

    Invalid API key protection (throw 401)

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 class ApiController < ActionController::API
+  before_action :require_api_key
 
   def current_player
     if request.headers['X-API-KEY'] == current_game.player_1.api_key
@@ -26,6 +27,14 @@ class ApiController < ActionController::API
 
   def current_game
     @game ||= Game.find(params[:game_id])
+  end
+
+  private
+
+  def require_api_key
+    user = User.find_by_api_key(request.headers['X-API-Key'])
+
+    render json: {message: 'Unauthorized'}.to_json, status: 401 and return false if user.nil?
   end
 
 end
