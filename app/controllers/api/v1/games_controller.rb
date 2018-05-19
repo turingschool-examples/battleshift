1fdf4d276e3b44d767c03a2ed93fd030b570e0d7
pollockcl <pollock.clee@gmail.com>
2018-05-18 19:50:30 -0600

    some slight refactors

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,27 +1,22 @@
 module Api
   module V1
-    class GamesController < ActionController::API
+    class GamesController < ApiController 
+      before_action :find_game, only: [:create]
+      
       def show
-        game = Game.find_by_id(params[:id])
-        if game
-          render json: game
-        else
-          render json: not_found, status: 400
-        end
+        render_400('Game does not exist') unless game
+        render_game
       end
-
 
       def create
         render json: Game.create_default(params, request.headers)
       end
 
-
       private
 
-        def not_found
-          { status: 400,
-            message: 'Game does not exist' }.to_json
-        end
+      def render_game
+        render json: game
+      end
     end
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,11 @@
 class ApiController < ActionController::API
+  protected
+
+  def find_game
+    game = Game.find_by_id(params[:id])
+  end
+
+  def render_400(message)
+    render json: message, status: 400
+  end
 end
