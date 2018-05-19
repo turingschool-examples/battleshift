a99e30a5bc9b269d18ee3f8e2f9e102ab2ac9d12
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-18 23:14:54 -0600

    Validate turn hook in before action for shots controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -2,12 +2,19 @@ module Api
   module V1
     module Games
       class ShotsController < ApiController
+        before_action :validate_turn, only: :create
+
         def create
           game = Game.find(params[:game_id])
           turn_processor = TurnProcessor.new(game, params[:shot][:target], opponent_board)
 
           turn_processor.run!
           render json: game, message: turn_processor.message
+        end
+
+        def validate_turn
+          response = { message: "Invalid move. It's your opponent's turn" }.to_json
+          render json: response, status: 400 and return false if current_turn != current_user
         end
       end
     end
