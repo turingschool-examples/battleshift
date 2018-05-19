b413d4af6f68f555b5d6b28079d8b3fb74d76391
pollockcl <pollock.clee@gmail.com>
2018-05-19 12:14:21 -0600

    cleaned up the controller a little more

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -3,33 +3,36 @@ module Api
     module Games
       class ShotsController < ApiController
         before_action :find_game, only: [:create]
+        before_action :validate,  only: [:create]
 
         def create
-          return render_401 unless user
-          return render_400("Invalid move. It's your opponent's turn") if @game.invalid_turn?(api_key) 
-          return render_400('Invalid move. Game over.') unless @game.in_progress?
-          return render_400(turn_processor.message) unless turn_processor.run! == true
-          render json: @game, message: turn_processor.message
+          render json: @game, message: processor.message
         end
 
         private
+
+        def validate
+          return render_401 unless user
+          return render_400 if @game.invalid_turn?(api_key)
+          return render_400('Invalid move. Game over.') unless @game.in_progress?
+          return render_400(processor.message) unless processor.run! == true
+        end
 
         def user 
           User.find_by_api_key(api_key)
         end
 
-        def turn_processor
-          @turn_processor ||= TurnProcessor.new(@game, params[:shot][:target])
+        def processor
+          @processor ||= TurnProcessor.new(@game, params[:shot][:target])
         end
 
         def render_401
           render json: @game, message: "Unauthorized", status: 401
         end
 
-        def render_400(message)
+        def render_400(message = "Invalid move. It's your opponent's turn")
           render json: @game, message: message, status: 400
         end
-
       end
     end
   end
