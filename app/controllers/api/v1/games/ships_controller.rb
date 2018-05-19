5693d75b3ccbcd1f356f3aababb5cf37e5356e0b
pollockcl <pollock.clee@gmail.com>
2018-05-18 20:08:55 -0600

    abstracted the board setting

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -3,24 +3,31 @@ module Api
     module Games
       class ShipsController < ApiController 
         before_action :find_game, only: [:index]
+        before_action :set_board, only: [:index]
 
         def index
-          if api_key == @game.player_1.api_key
-            board = @game.player_1_board
-          else
-            board = @game.player_2_board
-          end
           length = params[:ship_size]
           start_space = params[:start_space]
           end_space = params[:end_space]
           ship = Ship.new(length)
           ship.place(start_space, end_space)
   
-          ship_placer = ShipPlacer.new(board: board, ship: ship, start_space: params['start_space'], end_space: params['end_space'])
+          ship_placer = ShipPlacer.new(board: @board, ship: ship, start_space: params['start_space'], end_space: params['end_space'])
           ship_placer.run
           message = ship_placer.message #call the message from the ShipPlacer
           @game.save!
           render json: @game, message: message
+        end
+
+
+        private
+
+        def set_board
+          if api_key == @game.p1.api_key
+            @board = @game.p1_board
+          else
+            @board = @game.p2_board
+          end
         end
       end
     end
