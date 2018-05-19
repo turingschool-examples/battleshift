f44caee5603524b966747844945778bed95082e5
pollockcl <pollock.clee@gmail.com>
2018-05-18 20:24:28 -0600

    finished refactoring the ships_controller, for now

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -6,29 +6,36 @@ module Api
         before_action :set_board, only: [:index]
 
         def index
-          length = params[:ship_size]
-          start_space = params[:start_space]
-          end_space = params[:end_space]
-          ship = Ship.new(length)
-          ship.place(start_space, end_space)
-  
-          ship_placer = ShipPlacer.new(board: @board, ship: ship, start_space: params['start_space'], end_space: params['end_space'])
+          ship.place(params[:start_space], params[:end_space] )
           ship_placer.run
-          message = ship_placer.message #call the message from the ShipPlacer
           @game.save!
           render json: @game, message: message
         end
 
-
         private
 
-        def set_board
-          if api_key == @game.p1.api_key
-            @board = @game.p1_board
-          else
-            @board = @game.p2_board
+          def ship_placer
+            ShipPlacer.new( board: @board,
+                            ship: ship,
+                            start_space: params[:start_space],
+                            end_space: params[:end_space] )
           end
-        end
+
+          def message
+            ship_placer.message
+          end
+
+          def ship
+            Ship.new(params[:ship_size])
+          end
+
+          def set_board
+            if api_key == @game.p1.api_key
+              @board = @game.p1_board
+            else
+              @board = @game.p2_board
+            end
+          end
       end
     end
   end
