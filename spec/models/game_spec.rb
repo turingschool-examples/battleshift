5ef408747549e06f45bef10dae9f78a9abe23d44
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-20 08:48:02 -0600

    Adds relationships test for game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should validate_presence_of(:player_1) }
+    it { should validate_presence_of(:player_2) }
+  end
 end
