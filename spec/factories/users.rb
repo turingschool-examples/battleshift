59256fd0e6e34422cc08c251048cee23f5825154
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-20 15:33:58 -0600

    Improves user factory and begins testing of turn processor

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
     name "#{Faker::HarryPotter.character}"
-    email 'test@test.com'
+    sequence(:email) { |email| "#{email}#{Faker::Internet.email}"}
     password 'test'
     status 0
+    api_key SecureRandom.urlsafe_base64
   end
 end
