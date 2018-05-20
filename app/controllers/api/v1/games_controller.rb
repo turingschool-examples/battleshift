c60955a83bb03d8e76a410df9fa63ec616ca7ed5
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-20 13:26:26 -0600

    Refactoring validations.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class GamesController < ActionController::API
+    class GamesController < ApiController
       def show
         game = Game.find(params[:id])
         render json: game
@@ -11,6 +11,7 @@ module Api
       def create
         player_1_key = request.headers['X-API-KEY']
         player_2_key = User.find_by(email: params['opponent_email']).api_key
+
         player_1 = Player.new(Board.new(4), player_1_key)
         player_2 = Player.new(Board.new(4), player_2_key)
 
