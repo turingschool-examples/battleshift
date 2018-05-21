574057df5675d626c3585bf3a564e03d0a2f0622
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-20 22:07:02 -0600

    Fixes shots_spec to work with new setup

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :game do
-    player_1_board "You should add a board object"
-    player_2_board "You should add a board object"
-    winner nil
-    player_1_turns 0
-    player_2_turns 0
-    current_turn "challenger"
+    player_1 create(:user)
+    player_2 create(:user)
+    player_1_board Board.new(4)
+    player_2_board Board.new(4)
   end
 end
