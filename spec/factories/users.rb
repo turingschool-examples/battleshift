72da1f115f36379c46077777f387f23c52256161
Jliv316 <jclivingston316@gmail.com>
2018-07-10 15:34:23 -0600

    fixes factory bot username

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username "#{{Faker::GameOfThrones.unique.house}}@gmail.com"
+    username {Faker::Name.unique.first_name}
     first_name {Faker::Name.unique.first_name}
     last_name {Faker::Food.ingredient}
     api_key {Faker::Config.random.seed}
--- spec/mailers/battleship_notifier_spec.rb
+++ spec/mailers/battleship_notifier_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe BattleshipNotifierMailer, type: :mailer do
   describe 'user recieves email with appropriate subject, body, and link' do
-    let(:user){mock_model User, first_name: 'John', last_name: 'Livingston', email: 'jliv316@gmail.com'}
+    let(:user){ create(:user) }
     let(:mail){BattleshipNotifierMailer.welcome(user)}
 
     it 'renders the subject' do
@@ -10,7 +10,7 @@ RSpec.describe BattleshipNotifierMailer, type: :mailer do
     end
 
     it 'renders the reciever email' do
-      expect(mail.to).to eq([user.email])
+      expect(mail.to).to eq([user.username])
     end
 
     it 'renders the senders email' do
