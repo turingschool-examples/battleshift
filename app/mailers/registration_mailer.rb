0464a68a2f18f6a7f6d870f6660e61adb734b6b4
JamisonOrdway <jamisonordway@gmail.com>
2018-07-10 10:29:58 -0600

    Changes URL

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,6 +2,11 @@ class RegistrationMailer < ApplicationMailer
 
   def activate(user)
     @api_key = user.api_key
+    if ENV["RAILS_ENV"] == 'production'
+      @url = 'http://battle-shift-noobs.herokuapp.com'
+    else
+      @url = 'localhost:3000'
+    end 
     mail(to: user.email, subject: "Welcome to Battleshift!")
   end
 
