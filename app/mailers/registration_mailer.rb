b08cfca2edf6b50388a8e835f9b1feb568f212e0
apoc64 <sschwedt@gmail.com>
2018-07-09 22:23:10 -0600

    calls generate api key to display in email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,7 +2,7 @@ class RegistrationMailer < ApplicationMailer
 
   def activate(user)
     @api_key = user.api_key
-    mail(to: user.email, subject: "Welcome to Battleshift!")   
+    mail(to: user.email, subject: "Welcome to Battleshift!")
   end
 
-end  
+end
