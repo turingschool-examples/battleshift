bcc870ab30beacb496802ea02325770e4f3c87f3
Jliv316 <jclivingston316@gmail.com>
2018-07-10 14:58:48 -0600

    adds unique email to user factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username {Faker::GameOfThrones.unique.house}
+    username "#{{Faker::GameOfThrones.unique.house}}@gmail.com"
     first_name {Faker::Name.unique.first_name}
     last_name {Faker::Food.ingredient}
     api_key {Faker::Config.random.seed}
