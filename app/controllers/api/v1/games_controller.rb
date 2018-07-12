e910b78e7fc359238db4e4485a23307f40f6f0d4
Jliv316 <jclivingston316@gmail.com>
2018-07-12 16:18:36 -0600

    adds user invite feature tests

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -8,15 +8,17 @@ module Api
 
       def create
         p1 = User.find_by(api_key: game_params[:api_key])
-        player_not_activated(p1) if !p1.activated
         p2 = User.find_by(username: game_params[:player_2_username])
         if p2.nil?
           player_2_not_registered(game_params[:player_2_username], p1)
-          exit
+        elsif !p2.activated
+          player_not_activated(p2)
+        elsif !p1.activated
+          player_not_activated(p1)
+        else
+          game = Game.create(player_1_id: p1, player_2_id: p2, player_1_api_key: p1.api_key, player_2_api_key: p2.api_key)
+          invite_player_2(p2, game)
         end
-        player_not_activated(p2) if !p2.activated
-        game = Game.create(player_1_id: p1, player_2_id: p2, player_1_api_key: p1.api_key, player_2_api_key: p2.api_key)
-        invite_player_2(p2, game)
       end
 
       private
@@ -27,10 +29,10 @@ module Api
 
       def player_not_activated(player)
         BattleshipNotifierMailer.welcome(player, request.base_url).deliver_now
-        if player == p1
-          render json: { message: "You must activate your account before you can play a game. Check your email for another activation email."}, status: 400
-        else
+        if player.username == game_params[:player_2_username]
           render json: { message: "You're opponent must activate their account before you can play a game. They have been sent an activation email."}, status: 400
+        else
+          render json: { message: "You must activate your account before you can play a game. Check your email for another activation email."}, status: 400
         end
       end
 
