85158a5472e8a2d381b61d5082040fc58412dc0a
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-12 14:57:59 -0600

    Implements test for player 2 not being registered

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -10,9 +10,12 @@ module Api
         p1 = User.find_by(api_key: game_params[:api_key])
         player_not_activated(p1) if !p1.activated
         p2 = User.find_by(username: game_params[:player_2_username])
-        player_2_not_registered(game_params[:player_2_username]) if p2.nil?
+        if p2.nil?
+          player_2_not_registered(game_params[:player_2_username], p1)
+          exit
+        end
         player_not_activated(p2) if !p2.activated
-        game = Game.create(player_1_id: p1.id, player_2_id: p2.id, player_1_api_key: p1.api_key, player_2_api_key: p2.api_key)
+        game = Game.create(player_1_id: p1, player_2_id: p2, player_1_api_key: p1.api_key, player_2_api_key: p2.api_key)
         invite_player_2(p2, game)
       end
 
@@ -21,7 +24,7 @@ module Api
       def game_params
         params.require(:game).permit(:api_key, :player_2_username)
       end
-      
+
       def player_not_activated(player)
         BattleshipNotifierMailer.welcome(player, request.base_url).deliver_now
         if player == p1
@@ -30,14 +33,14 @@ module Api
           render json: { message: "You're opponent must activate their account before you can play a game. They have been sent an activation email."}, status: 400
         end
       end
-      
-      def player_2_not_registered(p2_email)
-        BattleshipNotifierMailer.register(p2_email).deliver_now
-        render json: { message: "Your oponent must create an account before you can play a game. They can check their email for a registration link."}, status: 400
+
+      def player_2_not_registered(p2_email, p1)
+        BattleshipNotifierMailer.register(p2_email, p1).deliver_now
+        render json: { message: "Your opponent must create an account before you can play a game. They can check their email for a registration link."}, status: 400
       end
 
       def invite_player_2(p2, game)
-        BattleshipNotifierMailer.invitation(p2, request.base_url).deliver_now
+        # BattleshipNotifierMailer.invitation(p2, request.base_url).deliver_now
         render json: { message: "#{p2.first_name} has been invited to play a game of Battleship with you.", id: "#{game.id}" }
       end
     end
--- spec/features/registered_user_can_create_a_new_game_with_opponent_spec.rb
+++ spec/features/registered_user_can_create_a_new_game_with_opponent_spec.rb
@@ -8,10 +8,10 @@ describe 'Registered User', type: :request do
     it 'can create a new game by sending a post request with an API key and another players username' do
       post "/api/v1/games", params: { game: { api_key: user1.api_key, player_2_username: user2.username } }
 
-      expect(response.code).to eq(200)
-      expect(Game.last.player_1_id).to eq(user1.id) 
+      expect(response.code).to eq("200")
+      expect(Game.last.player_1_id).to eq(user1) 
       expect(Game.last.player_1_api_key).to eq(user1.api_key) 
-      expect(Game.last.player_2_id).to eq(user2.id) 
+      expect(Game.last.player_2_id).to eq(user2) 
       expect(Game.last.player_2_api_key).to eq(user2.api_key) 
     end
 
@@ -19,7 +19,7 @@ describe 'Registered User', type: :request do
       post "/api/v1/games", params: { game: { api_key: user1.api_key, player_2_username: "example@example.com" } }
 
       expect(response.code).to eq(400)
-      expect(response.body[:message]).to eq("Your oponent must create an account before you can play a game. They can check their email for a registration link.")
+      expect(response.body[:message]).to eq("Your opponent must create an account before you can play a game. They can check their email for a registration link.")
     end
   end
 end
