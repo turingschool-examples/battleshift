e47b0184e4b0e56637888b78546e093dc2ee2fcb
apoc64 <sschwedt@gmail.com>
2018-07-14 14:18:52 -0600

    controller helper can place ships

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -2,47 +2,44 @@ class Api::V1::Games::ShipsController < ApiController
   def create
     key = request.env["HTTP_X_API_KEY"]
     game_id = params[:game_id]
-    if ch = ControllerHelper.new(key, game_id)
-      puts "ch created"
-    else
-      puts "ch failed"
-    end
-    # ship_params = {
-    #   ship_size: params[:ship_size],
-    #   start_space: params[:start_space],
-    #   end_space: params[:end_space]
-    # }
+    ch = ControllerHelper.new(key, game_id)
+    game = ch.place_ship(ship_params)
+    # binding.pry
     # sp = ShipPlacer.new(key, game_id, ship_params)
     # game = sp.run
     # render json: game
-
-    user = User.find_by(api_key: key)
-    game = Game.find(params[:game_id])
-    if game.player_1 == user
-      board = game.player_1_board
-    elsif game.player_2 == user
-      board = game.player_2_board
-    else
-      render json: {"message" => "Bad request"}, status: 400
-    end
-
-    ship = Ship.new(params[:ship_size].to_i)
-
-    ShipPlacer.new(
-      board: board,
-      ship: ship,
-      start_space: params[:start_space],
-      end_space: params[:end_space]
-    ).run
-    game.save
-    game.set_message("Successfully placed ship with a size of #{ship.length}. You have #{board.left_to_place} ship(s) to place#{board.length_of_remaining_ship}.")
+    #
+    # user = User.find_by(api_key: key)
+    # game = Game.find(params[:game_id])
+    # if game.player_1 == user
+    #   board = game.player_1_board
+    # elsif game.player_2 == user
+    #   board = game.player_2_board
+    # else
+    #   render json: {"message" => "Bad request"}, status: 400
+    # end
+    #
+    # ship = Ship.new(params[:ship_size].to_i)
+    #
+    # ShipPlacer.new(
+    #   board: board,
+    #   ship: ship,
+    #   start_space: params[:start_space],
+    #   end_space: params[:end_space]
+    # ).run
+    # game.save
+    # game.set_message("Successfully placed ship with a size of #{ship.length}. You have #{board.left_to_place} ship(s) to place#{board.length_of_remaining_ship}.")
 
     render json: game
   end
 
-  # private
-  #
-  # def ship_params
-  #   params.require(:ship).permit(:ship_size, :start_space, :end_space)
-  # end
+  private
+
+  def ship_params
+    {
+      ship_size: params[:ship_size].to_i,
+      start_space: params[:start_space],
+      end_space: params[:end_space]
+    }
+  end
 end
--- app/services/controller_helper.rb
+++ app/services/controller_helper.rb
@@ -10,6 +10,41 @@ class ControllerHelper
       @valid = true
     end
   end
+
+  def place_ship(params)
+    return {"message" => "Bad request"} unless valid
+    my_board = true
+    ship = Ship.new(params[:ship_size])
+    board = find_board(my_board)
+    sp = ShipPlacer.new(
+      board: board,
+      ship: ship,
+      start_space: params[:start_space],
+      end_space: params[:end_space]
+    )
+    r = sp.run
+    # binding.pry
+    @game.set_message("Successfully placed ship with a size of #{ship.length}. You have #{board.left_to_place} ship(s) to place#{board.length_of_remaining_ship}.")
+    @game.save
+    @game
+  end
+
+  def fire_shot(params)
+    return {"message" => "Bad request"} unless valid
+    my_board = false
+    board = find_board(my_board)
+  end
+
+  private
+
+  def find_board(my_board)
+    # Returns the appropriate board for ship placement or firing at
+    if (@user == @game.player_1) == my_board
+      return @game.player_1_board
+    else
+      return @game.player_2_board
+    end
+  end
 end
 
     # user = User.find_by(api_key: key)
@@ -21,3 +56,23 @@ end
     # else
     #   render json: {"message" => "Bad request"}, status: 400
     # end
+    # if game.player_1 == user
+    #   board = game.player_1_board
+    # elsif game.player_2 == user
+    #   board = game.player_2_board
+    # else
+    #   render json: {"message" => "Bad request"}, status: 400
+    # end
+    #
+    # ship = Ship.new(params[:ship_size].to_i)
+    #
+    # ShipPlacer.new(
+    #   board: board,
+    #   ship: ship,
+    #   start_space: params[:start_space],
+    #   end_space: params[:end_space]
+    # ).run
+    # game.save
+    # game.set_message("Successfully placed ship with a size of #{ship.length}. You have #{board.left_to_place} ship(s) to place#{board.length_of_remaining_ship}.")
+    #
+    # render json: game
