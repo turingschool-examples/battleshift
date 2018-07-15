dd800b2ac359d36934d483abeefc5731317e0752
apoc64 <sschwedt@gmail.com>
2018-07-15 13:17:32 -0600

    user cannot place additional ship if board is full

--- app/services/controller_helper.rb
+++ app/services/controller_helper.rb
@@ -17,8 +17,9 @@ class ControllerHelper
   def place_ship(params)
     return invalid_user unless valid
     my_board = true
-    ship = Ship.new(params[:ship_size])
     board = find_board(my_board)
+    return invalid_game("You have already placed all your ships.", 400) if ships_placed?(board)
+    ship = Ship.new(params[:ship_size])
     ShipPlacer.new(
       board: board,
       ship: ship,
@@ -43,45 +44,14 @@ class ControllerHelper
     game
   end
 
-  # if user != game.player_1 && user != game.player_2
-  #   return render json: {"message" => "Unauthorized"}, status: 401
-  # end
-  #
-  # if game.player_2 == user && game.current_turn == "player 2"
-  #   board = game.player_1_board
-  #   game.current_turn = "player 1"
-  # elsif game.player_1 == user && game.current_turn == "player 1"
-  #   board = game.player_2_board
-  #   game.current_turn = "player 2"
-  # else
-  #   game.set_message("Invalid move. It's your opponent's turn")
-  #
-  #   return render json: game, status: 400
-  # end
-  #
-  # if game.winner
-  #   game.set_message("Invalid move. Game over.")
-  #   return render json: game, status: 400
-  # end
-  #
-  # turn_processor = TurnProcessor.new(game, board, params[:target])
-  #
-  # turn_processor.run!
-  # message = game.set_message(turn_processor.message)
-  # if message.include?("Battleship sunk. Game over")
-  #   game.winner = user.id
-  #   game.save
-  # end
-  # if message.include?("Invalid coordinates")
-  #   status = 400
-  # else
-  #   status = 200
-  # end
-  #
-  # render json: game, status: status
   private
+
   attr_reader :game,
               :user
+
+  def ships_placed?(board)
+    board.left_to_place == 0
+  end
 
   def find_board(my_board)
     # Returns the appropriate board for ship placement or firing at for a specific user
