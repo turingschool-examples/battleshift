bb647273f11f9c0e4407ca2073464e32e20a4b82
jtrtj <jt.r@live.com>
2018-08-27 17:10:42 -0600

    working on creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,31 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(user_token: session[:user_token])
+  end
+  
   def new
     @user = User.new
   end
 
-  def def create
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "User successfully created"
-      redirect_to @user
+      session[:user_token] = @user.user_token
+      redirect_to dashboard_path
+      #send an email that includes user_token
     else
       flash[:error] = "Something went wrong"
       render 'new'
     end
+  end
+
+  def update
+    #change status to active after clicking email link
+    user = User.find_by(user_token: params[:user_token])
+    user.update!(status: 1)
+    session[:user_token] = user.user_token
+    redirect_to dashboard_path
   end
   
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,5 @@ describe User do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:status) }
-
   end
 end
