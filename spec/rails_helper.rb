5f035ffd71f1a11a28e6e9398f637e2a2a8c3e0b
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-27 13:42:15 -0600

    updates rails_helper file with database cleaner and shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,23 @@ end
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
