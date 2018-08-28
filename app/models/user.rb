ec535e7084c2679a5100a0048b98fb816e19b28b
JP Lynch <JPLynch35@gmail.com>
2018-08-28 10:12:14 -0600

    add user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :status
+  validates_presence_of :password, require: true
+  validates :password, confirmation: { case_sensitive: true }
+  validates :email, uniqueness: true, presence: true
+  has_secure_password
+
 end
