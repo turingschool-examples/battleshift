f6d47db6b6b24c0f700b5874de072628706a82de
JP Lynch <JPLynch35@gmail.com>
2018-08-28 10:57:57 -0600

    add feature test for failed login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -18,5 +18,18 @@ describe 'a user' do
       expect(page).to have_content('Logged in as Bob')
       expect(page).to have_content('This account has not yet been activated. Please check your email.')
     end
+    it 'cannot register n account without all required information' do
+      visit '/'
+      click_on 'Register'
+
+      expect(current_path).to eq('/register')
+
+      fill_in :user_name, with: 'Bob'
+      fill_in :user_password, with: 'Ben'
+      fill_in :user_password_confirmation, with: 'Ben'
+      click_on 'Submit'
+
+      expect(page).to have_content('Create a new account')
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ describe User, type: :model do
     it { should validate_presence_of(:status) }
   end
 
-  it 'exists' do
+  it 'exists and defaults to inactive' do
     user = User.new(name: 'Angela', email: 'Bob@Bob.Bob', password: 'Bob')
 
     expect(user).to be_a(User)
