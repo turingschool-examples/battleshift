6bb56c4cdfff0ac711a94ba246d89832777ac829
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-27 18:46:58 -0600

    add gems, build out user, controllers, routes, views for registration spec

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,37 +1,25 @@
 require 'rails_helper'
-# As a guest user
-# When I visit "/"
-# And I click "Register"
-# Then I should be on "/register"
-# And when I fill in an email address (required)
-# And I fill in name (required)
-# And I fill in password and password confirmation (required)
-# And I click submit
-# Then I should be redirected to "/dashboard"
-# And I should see a message that says "Logged in as <SOME_NAME>"
-# And I should see "This account has not yet been activated. Please check your email."
 
 describe 'visitor' do 
   context 'visiting the root path' do
     it 'can create a new account' do 
-      name = 'sample name'
+      name = 'Sample name'
       email = 'sample@email.com'
       password = 'sample password'
 
       visit '/'
-
+      
       click_button 'Register'
 
       expect(current_path).to eq('/register')
-      
-        fill_in :user_name, with: name
-        fill_in :user_email, with: email
-        fill_in :user_password, with: password
-
-      click_button 'Submit'
+        fill_in 'user[name]', with: name
+        fill_in 'user[email]', with: email
+        fill_in 'user[password]', with: password
+        fill_in 'user[password_confirmation]', with: password
+        click_button 'Submit'
 
       expect(current_path).to eq(dashboard_path)
-      expect(page).to have_content("Logged in as #{name}!")
+      expect(page).to have_content("Logged in as #{name}")
       expect(page).to have_content('This account has not yet been activated. Please check your email.')
     end
   end
