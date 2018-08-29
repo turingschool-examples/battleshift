c0aa2e7ac1e7014b6e33f2da21708bd224bd9422
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-29 15:36:10 -0600

    add game create post method to game route

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,11 +1,20 @@
+require "./app/controllers/api_controller.rb"
+
 module Api
   module V1
     class GamesController < ActionController::API
-      before_action :authenticate
-      def create
-        auth_token = request.headers["HTTP_X_API_KEY"]
-      end
+      before_action :authenticate_token, :authenticate_email
 
+      def create
+        game = Game.new({
+          player_1_board: Board.new(4),
+          player_2_board: Board.new(4),
+          player_1_turns: 0,
+          player_2_turns: 0,
+          current_turn: "challenger"
+        })
+        game.save!
+      end
 
       def show
         # TODO: params[:auth_token] instead?
@@ -16,8 +25,25 @@ module Api
         end
       end
 
+      private
 
+      def authenticate_token
+        api_key = request.headers['auth_token']
+        @user = User.where(auth_token: api_key).first if api_key
+        unless @user
+          head status: :unauthorized
+          return false
+        end
+      end
 
+      def authenticate_email
+        email = params[:email]
+        user = User.where(email: email).first if email
+        unless user
+          head status: :unauthorized
+          return false
+        end
+      end
     end
   end
 end
--- spec/requests/create_game_spec.rb
+++ spec/requests/create_game_spec.rb
@@ -2,29 +2,18 @@ require 'rails_helper'
 
 describe 'post api/v1/games' do
   it 'can create a new game' do
-    test_user = create(:user)
-    second_user = create(:user, auth_token: "kasvkb495489", email: "fakeymcfake@fake.com")
+    user_1 = create(:user)
+    user_2 = create(:user, auth_token: "kasvkb495489", email: "fakeymcfake@fake.com")
 
+    headers = { "CONTENT_TYPE" => "application/json", "auth_token" => "#{user_1.auth_token}"}
+    json_payload = {email: "#{user_2.email}"}.to_json
 
+    post = post "/api/v1/games", params: json_payload, headers: headers
 
-    endpoint = "/api/v1/games"
-    #TODO: how do we json post with a body of test_user.auth_token
-    Game.create(
-      player_1_auth_token: params[:auth_token]
-      player_2_email: params[:email]
-    )
+    game = Game.last
 
-    headers = { "CONTENT_TYPE" => "application/json", "auth_token" => "#{test_user.auth_token}"}
-    json_payload = {email: "#{user.email}"}.to_json
-
-
-
-
-
-    post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: headers
+    expect(Game.count).to eq(1)
+    expect(game).to have_key(:player_1_auth_token)
+    expect(game).to have_key(:player_2_auth_token)
   end
-
-
-
-
 end
