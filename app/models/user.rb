0ab4d2e7ea1b3865121452a95ffb21ffba553204
ClaireBvs <clairembeauvais@gmail.com>
2018-08-29 13:46:56 -0600

    add private method generate email token to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
 
   validates_presence_of :name
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, allow_nil: true
-end
 
+  private
+
+  def generate_email_token
+    if self.email_token.blank?
+      self.email_token = SecureRandom.hex(10)
+    end
+  end
+end
