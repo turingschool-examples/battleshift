a9d25c0dd4cda481654d5d8429d41747f0fa0afc
ClaireBvs <clairembeauvais@gmail.com>
2018-08-29 13:41:26 -0600

    add routing confirmation email

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'welcome#index'
-  
+
   resources :users
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
   get '/dashboard', to: 'dashboard#show'
+
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
 
   namespace :api do
     namespace :v1 do
