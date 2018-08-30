00b0bdaf8e2002fbad74c588bb66703970130d73
brickstar <brickstar@gmail.com>
2018-08-30 16:36:13 -0600

    add turn switcher, player_id

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,23 @@
 class Game < ApplicationRecord
+  belongs_to :player_1, class_name: "User", foreign_key: "player_1_id"
+  belongs_to :player_2, class_name: "User", foreign_key: "player_2_id"
   attr_accessor :messages
 
   enum current_turn: ["player 1", "player 2"]
   serialize :player_1_board
   serialize :player_2_board
-  
-  validates :player_1_token, presence: true
-  validates :player_2_token, presence: true
-  
+
+  validates :player_1_id, presence: true
+  validates :player_2_id, presence: true
+
   validates :player_1_board, presence: true
   validates :player_2_board, presence: true
+
+  def turn_switcher(user)
+    if user.id == player_1.id
+      update(current_turn: 1)
+    elsif user.id == player_2.id
+      update(current_turn: 0)
+    end
+  end
 end
