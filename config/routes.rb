058b4779da87194001c9387ae5a8390b433b51ec
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-29 20:39:14 -0600

    WIP setting up activation url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: "welcome#index"
-  get "/register", to: 'users#new'
+  root to: 'welcome#index'
+  get '/register', to: 'users#new'
+  get '/activate', to: 'activate#index'
+  get '/dashboard', to: 'users#show'
+  resources :users, only: [:new, :create]
 
   resources :users, except: [:index, :show, :new, :create, :edit, :update, :destroy] do
     member do
@@ -8,13 +11,10 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/dashboard', to: 'users#show'
-  resources :users, only: [:new, :create]
-
   namespace :api do
     namespace :v1 do
       resources :games, only: [:show] do
-        post "/shots", to: "games/shots#create"
+        post '/shots', to: 'games/shots#create'
       end
     end
   end
