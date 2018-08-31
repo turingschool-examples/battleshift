d60a858073c58666e2a679b3f5fff8066c549efe
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-30 21:40:15 -0600

    spec harness creates a game now

--- db/migrate/20180829205920_remove_api_key_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveApiKeyFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :api_key, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,8 @@ ActiveRecord::Schema.define(version: 20180829211544) do
     t.integer "player_1_turns"
     t.integer "player_2_turns"
     t.integer "current_turn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_tokens", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "token"
+    t.integer "player_id"
+    t.integer "opponent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
