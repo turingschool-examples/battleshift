1c9f06424bb6d6cf422a651242bfb2fa99ac1fff
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-09-01 23:33:13 -0600

    add ship amount validator to ship placer

--- spec/requests/players_can_place_ships_spec.rb
+++ spec/requests/players_can_place_ships_spec.rb
@@ -6,10 +6,12 @@ describe "a player" do
       @user_1 = create(:user)
       @user_2 = create(:user, auth_token: "kasvkb495489", email: "fakeymcfake@fake.com")
 
-      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_1.auth_token}"}
+      @player_1_headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_1.auth_token}" }
+      @player_2_headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_2.auth_token}" }
+
       json_payload = {opponent_email: "#{@user_2.email}"}.to_json
 
-      post "/api/v1/games", params: json_payload, headers: headers
+      post "/api/v1/games", params: json_payload, headers: @player_1_headers
 
       @game = Game.last
 
@@ -27,10 +29,8 @@ describe "a player" do
     end
 
     it "player 1 places a ship on their board" do
-      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_1.auth_token}"}
-      post "/api/v1/games/#{@game.id}/ships", params: @ship_1_payload, headers: headers
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_1_payload, headers: @player_1_headers
       payload = JSON.parse(response.body, symbolize_names: true)
-
       @game.reload
 
       expect(payload[:message]).to eq("Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2.")
@@ -40,12 +40,9 @@ describe "a player" do
     end
 
     it "player 1 places two ships on the board" do
-      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_1.auth_token}"}
-      post "/api/v1/games/#{@game.id}/ships", params: @ship_1_payload, headers: headers
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_1_payload, headers: @player_1_headers
       JSON.parse(response.body, symbolize_names: true)
-
-      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_1.auth_token}"}
-      post "/api/v1/games/#{@game.id}/ships", params: @ship_2_payload, headers: headers
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_2_payload, headers: @player_1_headers
       payload = JSON.parse(response.body, symbolize_names: true)
       @game.reload
 
@@ -54,9 +51,32 @@ describe "a player" do
       expect(@game.player_1_board.board.fourth.second["D2"].contents).to be_a(Ship)
     end
 
+    it "raises error when placing a ship on a preexisting ship" do
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_1_payload, headers: @player_1_headers
+      JSON.parse(response.body, symbolize_names: true)
+      @game.reload
+
+      ship_2_payload =  {
+                          ship_size: 2,
+                          start_space: "A1",
+                          end_space: "A2"
+                        }.to_json
+
+      expect{ post "/api/v1/games/#{@game.id}/ships", params: ship_2_payload, headers: @player_1_headers }.to raise_error(InvalidShipPlacement)
+    end
+
+    it "raises error when ship start and end points are incorrect" do
+      ship_1_payload =  {
+                          ship_size: 2,
+                          start_space: "A1",
+                          end_space: "A3"
+                        }.to_json
+
+      expect{ post "/api/v1/games/#{@game.id}/ships", params: ship_1_payload, headers: @player_1_headers }.to raise_error(InvalidShipPlacement)
+    end
+
     it "player 2 places a ship on board" do
-      headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => "#{@user_2.auth_token}"}
-      post "/api/v1/games/#{@game.id}/ships", params: @ship_2_payload, headers: headers
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_2_payload, headers: @player_2_headers
       payload = JSON.parse(response.body, symbolize_names: true)
       @game.reload
 
@@ -72,6 +92,23 @@ describe "a player" do
 
       expect(response).to have_http_status(401)
       expect(payload[:message]).to eq("Unauthorized")
+    end
+
+    it "can not place more than 2 ships" do
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_1_payload, headers: @player_1_headers
+      JSON.parse(response.body, symbolize_names: true)
+      post "/api/v1/games/#{@game.id}/ships", params: @ship_2_payload, headers: @player_1_headers
+      JSON.parse(response.body, symbolize_names: true)
+
+      ship_3_payload = {
+                          ship_size: 3,
+                          start_space: "C1",
+                          end_space: "C3"
+                        }.to_json
+
+      @game.reload
+
+      expect{ post "/api/v1/games/#{@game.id}/ships", params: ship_3_payload, headers: @player_1_headers }.to raise_error(InvalidShipPlacement)
     end
   end
 end
