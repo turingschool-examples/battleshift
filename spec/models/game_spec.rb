f8a73455d52947c39fa4f77ca00f367ad3f0385a
brickstar <brickstar@gmail.com>
2018-09-01 18:45:53 -0600

    pass existing tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe Game, type: :model do
 
   context 'instance methods' do
     it '#turn_switcher' do
-      game = create(:game)
-      
+      player_1 = create(:user)
+      player_2 = create(:user)
+      game = create(:game, player_1: player_1, player_2: player_2)
       player_1 = game.player_1
+      allow_any_instance_of(ApiController).to receive(:current_user).and_return(player_1)
 
       expect(game.current_turn).to eq("player 1")
 
