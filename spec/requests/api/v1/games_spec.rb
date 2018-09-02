4a15d9775ae3a86d1219ac70614f522ccdef4ded
Chris Powell <chris.ian.powell@gmail.com>
2018-09-02 17:57:11 -0600

    adds shot spec for two player game

--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User starts new game with opponent email' do
   context 'POST /api/v1/games' do
-    it 'creates a new game with player 1 api_key and opponent email 1' do
+    it 'creates a new game with player 1 api_key and opponent email' do
       user_1 = create(:user, active: true)
       user_2 = create(:user, active: true, username: 'blob', email: 'dude@dudeness.yo')
 
@@ -17,80 +17,6 @@ describe 'User starts new game with opponent email' do
       post "/api/v1/games", params: json_payload, headers: headers
 
       expect(Game.count).to eq(1)
-    end
-  end
-  context 'POST /api/v1/games/#{game_id}/ships' do
-    it 'can place ship with a valid api_key' do
-      user_1 = create(:user, active: true)
-      user_2 = create(:user, active: true, username: 'blob', email: 'dude@dudeness.yo')
-
-      game_attributes = {
-        player_1_board: Board.new(4),
-        player_2_board: Board.new(4),
-        player_1_turns: 0,
-        player_2_turns: 0,
-        player_key: user_1.api_token,
-        opponent_key: user_2.api_token
-      }
-
-      game = Game.create(game_attributes)
-
-      ship_1_payload = {
-      ship_size: 3,
-      start_space: "A1",
-      end_space: "A3"
-      }
-
-      headers = { "CONTENT_TYPE" => "application/json" }
-      headers['X-API-Key'] = user_1.api_token
-      ship_1_payload = {
-                        ship_size: 3,
-                        start_space: "A1",
-                        end_space: "A3"
-                       }.to_json
-
-      post "/api/v1/games/#{game.id}/ships", params: ship_1_payload, headers: headers
-
-      updated_game = Game.find(game.id)
-
-      expect(updated_game.player_1_board.board.first.first['A1'].contents).to be_a(Ship)
-      expect(updated_game.player_1_board.board.first[1]['A2'].contents).to be_a(Ship)
-      expect(updated_game.player_1_board.board.first[2]['A3'].contents).to be_a(Ship)
-    end
-    it 'will not place ship with an invalid api_key' do
-      user_1 = create(:user, active: true)
-      user_2 = create(:user, active: true, username: 'blob', email: 'dude@dudeness.yo')
-
-      game_attributes = {
-        player_1_board: Board.new(4),
-        player_2_board: Board.new(4),
-        player_1_turns: 0,
-        player_2_turns: 0,
-        player_key: user_1.api_token,
-        opponent_key: user_2.api_token
-      }
-
-      game = Game.create(game_attributes)
-
-      ship_1_payload = {
-      ship_size: 3,
-      start_space: "A1",
-      end_space: "A3"
-      }
-
-      headers = { "CONTENT_TYPE" => "application/json" }
-      headers['X-API-Key'] = "pOIUiuIIO890ihLKob"
-      ship_1_payload = {
-                        ship_size: 3,
-                        start_space: "A1",
-                        end_space: "A3"
-                       }.to_json
-
-      post "/api/v1/games/#{game.id}/ships", params: ship_1_payload, headers: headers
-
-      updated_game = Game.find(game.id)
-
-      expect(response.status).to eq(401)
     end
   end
 end
---
+++ spec/requests/api/v1/shots_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe 'Opponents play the game by firing at each others ships' do
+  context 'POST /api/v1/games/:game_id/shots' do
+    it 'controls the functionality of the gameplay' do
+      player = create(:user, active: true)
+      opponent = create(:user, active: true, username: 'blob', email: 'dude@dudeness.yo')
+
+      player_board = Board.new(4)
+      opponent_board = Board.new(4)
+
+      game_attributes = {
+        player_1_board: player_board,
+        player_2_board: opponent_board,
+        player_1_turns: 0,
+        player_2_turns: 0,
+        current_turn: 0,
+        player_key: player.api_token,
+        opponent_key: opponent.api_token
+      }
+
+      game = Game.new(game_attributes)
+
+      player_ship = {
+      ship_size: 3,
+      start_space: "A1",
+      end_space: "A3"
+      }
+
+      opponent_ship = {
+      ship_size: 3,
+      start_space: "A1",
+      end_space: "C1"
+      }
+
+      ShipPlacer.new(board: player_board,
+                     ship: player_ship,
+                     start_space: "A1",
+                     end_space: "A3"
+                    ).run
+
+      ShipPlacer.new(board: opponent_board,
+                     ship: opponent_ship,
+                     start_space: "A1",
+                     end_space: "C1"
+                    ).run
+
+      game.save
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+      headers['X-API-Key'] = player.api_token
+      json_payload = {target: "D1"}.to_json
+
+      post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: headers
+
+      body_json = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(body_json[:message]).to eq("Your shot resulted in a Miss.")
+      expect(body_json[:player_2_board][:rows][3][:data][0][:status]).to eq("Miss")
+    end
+  end
+end
