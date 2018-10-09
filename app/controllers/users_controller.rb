b7f667b60a6d0a3861e58a0eb569a90e3773fdf7
ClaireBvs <clairembeauvais@gmail.com>
2018-10-09 14:43:28 -0600

    add test for #all_users method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @search_result = UserPresenter.new
   end
 
   def show
--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -15,5 +15,13 @@ describe UserPresenter do
         expect(user_presenter.single_user_object).to be_a(UserDisplay)
       end
     end
+    context '#all_users' do
+      it 'should return an array of user_display objects' do
+        user_presenter = UserPresenter.new
+
+        expect(user_presenter.all_users).to be_a(Array)
+        expect(user_presenter.all_users.first).to be_a(UserDisplay)
+      end
+    end
   end
 end
