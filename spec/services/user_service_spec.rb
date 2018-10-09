8cdb800c737e8e061c3ef72166fdad903f457f69
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-09 15:22:15 -0600

    Adds vcr to user services

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -11,22 +11,26 @@ describe UserService do
   context 'instance method' do
     describe '#single_user_data' do
       it 'should return a hash' do
-        user_service = UserService.new({id: 1})
+        VCR.use_cassette("single_user_data") do
+          user_service = UserService.new({id: 1})
 
-        expect(user_service.single_user_data).to be_a(Hash)
-        expect(user_service.single_user_data).to have_key(:id)
-        expect(user_service.single_user_data).to have_key(:name)
-        expect(user_service.single_user_data).to have_key(:email)
+          expect(user_service.single_user_data).to be_a(Hash)
+          expect(user_service.single_user_data).to have_key(:id)
+          expect(user_service.single_user_data).to have_key(:name)
+          expect(user_service.single_user_data).to have_key(:email)
+        end
       end
     end
     describe '#all_user_data' do
       it 'returns the parsed response' do
-        user_service = UserService.new
+        VCR.use_cassette("all_user_data") do
+          user_service = UserService.new
 
-        expect(user_service.all_user_data).to be_a(Array)
-        expect(user_service.all_user_data.first).to have_key(:id)
-        expect(user_service.all_user_data.first).to have_key(:name)
-        expect(user_service.all_user_data.first).to have_key(:email)
+          expect(user_service.all_user_data).to be_a(Array)
+          expect(user_service.all_user_data.first).to have_key(:id)
+          expect(user_service.all_user_data.first).to have_key(:name)
+          expect(user_service.all_user_data.first).to have_key(:email)
+        end
       end
     end
   end
