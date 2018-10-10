dd6948cca2b91e6ec9a7ef0e211aacdd06e805d7
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-10 15:01:27 -0600

    /api/v1/users/ update method complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         post "/shots", to: "games/shots#create"
       end
 
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :update]
     end
   end
   resources :users, only: [:show, :index]
