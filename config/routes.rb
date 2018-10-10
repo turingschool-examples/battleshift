26aaae7e4e3ab333915a8b3c5d7ad4459ba5ca5f
ClaireBvs <clairembeauvais@gmail.com>
2018-10-10 10:12:01 -0600

    add routes and action update for api::V1::UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-
   namespace :api do
     namespace :v1 do
       resources :games, only: [:show] do
         post "/shots", to: "games/shots#create"
       end
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :update]
     end
   end
   resources :users, only: [:index, :show, :edit]
