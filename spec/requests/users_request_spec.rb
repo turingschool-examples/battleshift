8005f03a6d34fb476619625a870262231aef8fd6
ClaireBvs <clairembeauvais@gmail.com>
2018-10-10 10:09:11 -0600

    add request test for patch user/endpoint

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -37,4 +37,26 @@ describe "users api endpoints" do
       expect(returned_user).to have_key(:id)
     end
   end
+
+  context 'PATCH /api/v1/users/:id/' do
+    it 'update email for user' do
+      cj   = User.create!(name: "C.J. Cregg", id: 1, email: 'cj@example.com')
+      toby = User.create!(name: "Toby Ziegler", id: 2, email: 'toby@example.com')
+      old_email = cj.email
+      new_email = 'cregg@example.com'
+
+      patch "/api/v1/users/#{cj.id}", params: {email: new_email}
+
+      expect(response.status).to be 200
+
+      updated_user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(updated_user.keys.count).to eq(3)
+      expect(updated_user).to have_key(:name)
+      expect(updated_user).to have_key(:email)
+      expect(updated_user).to have_key(:id)
+      expect(updated_user[:email]).to_not eq(old_email)
+      expect(updated_user[:email]).to eq(new_email)
+    end
+  end
 end
