d71094f884e5288e2ea709ae1c9774306c7dee65
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-14 13:59:24 -0600

    Add email uniqueness validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email 
+  validates_presence_of :name, :email
+  validates_uniqueness_of :email
 
   validates :password, presence: true, on: :create
   validates_confirmation_of :password, on: :create
